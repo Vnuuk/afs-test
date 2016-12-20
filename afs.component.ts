3bcf7255722abe9cfbb4173f245b6f72cf9ab760
Vnuuk <noginanton@gmail.com>
2016-12-20 17:37:41 +0300

    update package

---
+++ afs.component.ts
@@ -0,0 +1,19 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'afs',
+    moduleId: module.id,
+    templateUrl: 'afs.component.html'
+})
+
+export class AfsComponent {
+    time: Date;
+
+    constructor() {
+        this.time = new Date();
+    }
+
+    updateTime(): void {
+        this.time = new Date();
+    }
+}
